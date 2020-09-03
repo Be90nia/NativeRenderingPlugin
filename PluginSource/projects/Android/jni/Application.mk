c3313e2287fea1468d307a416473004fb7e8d12c
kidapu <kidapu@gmail.com>
2020-09-03 23:54:46 +0900

    Add Plugin for Android

--- PluginSource/projects/Android/jni/Application.mk
+++ PluginSource/projects/Android/jni/Application.mk
@@ -1,5 +1,8 @@
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
-APP_PLATFORM := android-9
-APP_STL := gnustl_static
+APP_OPTIM        := debug
+APP_ABI := armeabi-v7a arm64-v8a 
+APP_PLATFORM := android-21
+# APP_STL := gnustl_static
+APP_STL          := c++_static
 APP_CPPFLAGS += -std=c++11
 NDK_TOOLCHAIN_VERSION := clang
+APP_BUILD_SCRIPT := Android.mk
